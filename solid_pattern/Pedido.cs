05e084ac5a77f4b4e9e6839041f3fbd00a951af7
jota-de <jose.villa494@pascualbravo.edu.co>
2025-04-27 10:02:02 -0500

    Definición: Clase Pedido

--- solid_pattern/Pedido.cs
+++ solid_pattern/Pedido.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace solid_pattern
 {
-    class Pedido
+    public class Pedido
     {
+        public string Cliente { get; set; }
+        public string Producto { get; set; }
+        public bool Urgente { get; set; }
+        public double Peso { get; set; }
+        public int Distancia { get; set; }
+        public IMetodoentrega MetodoEntrega { get; set; }
+        public Pedido(string cliente, string producto, bool urgente, double peso, int distancia)
+        {
+            Cliente = cliente;
+            Producto = producto;
+            Urgente = urgente;
+            Peso = peso;
+            Distancia = distancia;
+            MetodoEntrega = Factory.CrearEntrega(producto, urgente, peso);
+        }
+        public double ObtenerCosto() => MetodoEntrega.CalcularCosto(Distancia);
     }
 }
