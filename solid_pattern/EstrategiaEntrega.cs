91e07ae9d129b2d9c7b7300934a84604aac6b338
jota-de <jose.villa494@pascualbravo.edu.co>
2025-04-27 09:53:19 -0500

    Definición: Clase EstrategiaEntrega

---
+++ solid_pattern/EstrategiaEntrega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_pattern
+{
+    public class EstrategiaEntrega : IMetodoentrega
+    {
+        public double CalcularCosto(int km) => km * 20;
+        public string TipoEntrega() => "Dron";
+    }
+    public class EntregaMoto : IMetodoentrega {
+        public double CalcularCosto(int km) => km * 10;
+        public string TipoEntrega() => "Moto";
+    }
+    public class EntregaCamion: IMetodoentrega
+    {
+        public double CalcularCosto(int km) => km * 5;
+        public string TipoEntrega() => "Camion";
+    }
+   
+}
