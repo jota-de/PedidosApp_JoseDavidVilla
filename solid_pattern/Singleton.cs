933125648aeedc19ad988ba8ad19659ecacc9435
jota-de <jose.villa494@pascualbravo.edu.co>
2025-04-27 10:05:31 -0500

    Definición: Clase Singlenton

--- solid_pattern/Singleton.cs
+++ solid_pattern/Singleton.cs
@@ -8,5 +8,27 @@ namespace solid_pattern
 {
     class Singleton
     {
+        private static Singleton _instancia;
+        private static readonly object _lock = new object();
+        public List<Pedido> Pedidos { get; private set; }
+        private Singleton() => Pedidos = new List<Pedido>();
+        public static Singleton Instancia
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_instancia == null)
+                    {
+                        _instancia = new Singleton();
+                    }
+                    return _instancia;
+                }
+            }
+        }
+        public void AgregarPedido(Pedido pedido)
+        {
+            Pedidos.Add(pedido);
+        }
     }
 }
