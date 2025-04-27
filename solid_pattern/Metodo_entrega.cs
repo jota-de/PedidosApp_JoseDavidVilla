0cf7d19873eaaf665fe24e391bc57fa741a3d702
jota-de <jose.villa494@pascualbravo.edu.co>
2025-04-27 08:23:32 -0500

    Agregar archivos de proyecto.

---
+++ solid_pattern/Metodo_entrega.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace solid_pattern
+{
+    public interface IMetodoentrega
+    {
+        double CalcularCosto(int km);
+        string TipoEntrega();
+    }
+}
